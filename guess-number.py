f7edafdeb61fe47acd8a1b70ffeb525f5c95d950
LISA <fjlghb@gmail.com>
2024-03-03 21:24:17 +0800

    add count

--- guess-number.py
+++ guess-number.py
@@ -1,7 +1,10 @@
 import random
 r = random.randint(1, 100)
 
+count = 0
+
 while True:
+	count = count + 1
 	a = input('給你1~100的範圍,猜猜看我在想哪個數字~  ')
 	i_a = int(a)
 	if i_a == r:
@@ -11,3 +14,4 @@ while True:
 		print('比這個再小喔!')
 	else:
 		print('比這個再大喔!')
+	print('這是你猜的第', count, '次了喔')
